c5a5ca0c5ad373fed07a118fa2b2f657f3f298eb
beto1581 <nelsoncortezrueda1993@gmail.com>
2023-08-24 15:47:54 -0600

    word details style

--- components/Word-details.tsx
+++ components/Word-details.tsx
@@ -1,27 +1,60 @@
-import style from './Word-details.module.scss'
+import style from "./Word-details.module.scss";
 export default function WordDetails() {
-    return (
-        <div className={style.word_section}>
-            <div className={style.word_section__title}>Hello</div>
-            <div className={style.word_section__phonetic}>
-                <pre><code>həˈləʊ</code></pre>
-            </div>
-            <div className={style.word_section__origin}>
-                <p>origin</p>
-                <span>early 19th century: variant of earlier hollo ; related to holla.</span>
-            </div>
-            <div className={style.word_section__meanings}>
-                <p>meanings</p>
-                <div className={style.word_section__meaning__item}>
-                <span>partOfSpeech: exclamation</span>
-                <div className={style.word_section__meaning__item__definitions}>
-                    <span>definition: '-- --- --'</span>
-                    <span>example: '--'</span>
-                    <span>synonyms: '--'</span>
-                    <span>antonyms: '--'</span>
-                </div>
-                </div>
-            </div>
+  return (
+    <div className={style.word_section}>
+      <div className={style.word_section__title}>Hello</div>
+      <div className={style.word_section__phonetic}>
+        <pre>
+          <code>həˈləʊ</code>
+        </pre>
+      </div>
+      <div className={style.word_section__origin}>
+        <p>origin</p>
+        <span>
+          early 19th century: variant of earlier hollo ; related to holla.
+        </span>
+      </div>
+      <div className={style.word_section__meanings}>
+        <p>meanings</p>
+        <div className={style.word_section__meanings__item}>
+          <span className={style.word_section__meanings__item__title}>
+            <strong>partOfSpeech:</strong> exclamation
+          </span>
+          <div className={style.word_section__meanings__item__definitions}>
+            <span>
+              <strong>definition:</strong> '-- --- --'
+            </span>
+            <span>
+              <strong>example:</strong> '--'
+            </span>
+            <span>
+              <strong>synonyms:</strong> '--'
+            </span>
+            <span>
+              <strong>antonyms:</strong> '--'
+            </span>
+          </div>
         </div>
-    );
+        <div className={style.word_section__meanings__item}>
+          <span className={style.word_section__meanings__item__title}>
+            <strong>partOfSpeech:</strong> exclamation
+          </span>
+          <div className={style.word_section__meanings__item__definitions}>
+            <span>
+              <strong>definition:</strong> '-- --- --'
+            </span>
+            <span>
+              <strong>example:</strong> '--'
+            </span>
+            <span>
+              <strong>synonyms:</strong> '--'
+            </span>
+            <span>
+              <strong>antonyms:</strong> '--'
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
